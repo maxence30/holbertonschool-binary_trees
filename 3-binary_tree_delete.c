6c3d67624da365f08542e50488a13d830ccb9d25
maxence30 <henous.maxence@outlook.fr>
2025-12-11 08:48:29 +0000

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	/* Delete left subtree */
+	binary_tree_delete(tree->left);
+
+	/* Delete right subtree */
+	binary_tree_delete(tree->right);
+
+	/* Free current node */
+	free(tree);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -2,17 +2,9 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child
- * @right: Pointer to the right child
- */
+/* Structure */
 typedef struct binary_tree_s
 {
     int n;
@@ -21,11 +13,10 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Prototypes */
+/* PROTOTYPES */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
+void binary_tree_print(const binary_tree_t *tree);
+void binary_tree_delete(binary_tree_t *tree); /* ← AJOUT IMPORTANT */
 
 #endif /* BINARY_TREES_H */
