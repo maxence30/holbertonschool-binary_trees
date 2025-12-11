b7227f54dc76bb9ba13698c45428f696aec4ebcd
maxence30 <henous.maxence@outlook.fr>
2025-12-11 08:52:52 +0000

    task 4

--- binary_trees.h
+++ binary_trees.h
@@ -3,20 +3,29 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/* Structure */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
-/* PROTOTYPES */
+/* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *tree);
-void binary_tree_delete(binary_tree_t *tree); /* ← AJOUT IMPORTANT */
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
