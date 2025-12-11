0676f930f610b30b1217c2fbd00a9113b0d8008d
maxence30 <henous.maxence@outlook.fr>
2025-12-11 09:23:20 +0000

    exo 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node
+ * @tree: Pointer to the root of the tree
  *
- * Return: Balance factor or 0
+ * Return: Balance factor, or 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
@@ -14,9 +14,10 @@ int binary_tree_balance(const binary_tree_t *tree)
         return (0);
 
     if (tree->left)
-        left_h = 1 + binary_tree_height(tree->left);
+        left_h = (int)binary_tree_height(tree->left) + 1;
+
     if (tree->right)
-        right_h = 1 + binary_tree_height(tree->right);
+        right_h = (int)binary_tree_height(tree->right) + 1;
 
     return (left_h - right_h);
 }
